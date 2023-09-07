af1a31c830c8a7620f51c767a39ddddfe2d8363e
davidroulet <47849716+davidroulet@users.noreply.github.com>
2023-09-07 13:56:54 +0200

    Update start.sh
    add apt upgrade

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # Source Simony Cuany et David Roulet 
 # 07.09.2023
 apt update -y
-apt update -y
+apt upgrade -y
 apt install libz-dev libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext cmake gcc git -y
 
 git clone https://github.com/SimonCuany/LIN1.git
