937f3c2b504157d3b97132524fbc3438110c9a8e
davidroulet <47849716+davidroulet@users.noreply.github.com>
2023-09-07 13:53:35 +0200

    Update start.sh
    Remove the CD command there are useless

--- start.sh
+++ start.sh
@@ -1,19 +1,14 @@
 #! /bin/bash
 
 apt update -y
-apt install git -y
 apt update -y
-apt install libz-dev libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext cmake gcc -y
+apt install libz-dev libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext cmake gcc git -y
 
 git clone https://github.com/SimonCuany/LIN1.git
 
 chmod +x Lin1/debianInstall/testEnvironnement.sh
 
-cd Lin1/debianInstall/
-
-./testEnvironnement.sh
-
-cd ../../
+Lin1/debianInstall/testEnvironnement.sh
 
 rm -r Lin1
 
